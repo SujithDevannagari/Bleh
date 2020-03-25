b666a60663d09b1be2de31afe7263a83a6c7cf72
SujithDevannagari <sujith.d91@gmail.com>
2020-03-25 19:17:46 +0530

    first

---
+++ one.js
@@ -0,0 +1,14 @@
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+
+function myFunction() {
+  if (window.pageYOffset > 800) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
